9387ee54c083fc7653765b7dd8268095f89cfb7b
Rasta Mouse <rasta@rastamouse.me>
2023-01-18 17:44:51 +0000

    code formatting

--- TeamServer/Services/TaskService.cs
+++ TeamServer/Services/TaskService.cs
@@ -67,9 +67,10 @@ public sealed class TaskService : ITaskService
     public async Task<IEnumerable<TaskRecord>> GetPending(string droneId)
     {
         var conn = _db.GetAsyncConnection();
-        
-        var records = await conn.Table<TaskRecordDao>().Where(r =>
-            r.Status == (int)TaskStatus.PENDING && r.DroneId == droneId).ToArrayAsync();
+
+        var records = await conn.Table<TaskRecordDao>()
+            .Where(r => r.Status == (int)TaskStatus.PENDING && r.DroneId.Equals(droneId))
+            .ToArrayAsync();
 
         return records.Select(r => (TaskRecord)r);
     }
